17b9753061f1b08a5c44a56ed83acb3e8ac8c93a
suryaa6666 <suryaelidanto@gmail.com>
2022-06-09 11:40:27 +0700

    last one

---
+++ form.js
@@ -0,0 +1,14 @@
+// var gelas = "Teh";
+
+// console.log(gelas);
+
+// var gelas = "test";
+// console.log(gelas);
+
+// console.log("123"-50);
+
+
+const submitData = () => {
+    console.log('yahahaha hayuu');
+}
+
